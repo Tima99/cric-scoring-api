cc340610a59809fb8de60a82e64a01bae9561b6d
Tima <80810016+Tima99@users.noreply.github.com>
2022-12-10 15:52:48 +0530

    Remove duplicates opponent teams send to client

--- src/controllers/teams/opponentsTeam.js
+++ src/controllers/teams/opponentsTeam.js
@@ -4,22 +4,32 @@ export async function opponentsTeam(req, res){
     try {
         const email = req.email
         const myMatches = req.myMatches
+        const playerDoc = await Player.findOne({email: req.email})
 
         if( myMatches.length === 0) return res.send([])
         
         const playerNotFound = (player) => player._id !== email
 
+        const opponentsTeamId = []
+
         const opponentsTeam = myMatches.map( match => {
-            if( match.teamA.players && match.teamA.players.every(playerNotFound) )
+            const isExistA = opponentsTeamId.findIndex(teamId => (teamId === match.teamA._id) )
+            const isExistB = opponentsTeamId.findIndex(teamId => (teamId === match.teamB._id) )
+            if( isExistA === -1 && match.teamA.players && match.teamA.players.every(playerNotFound) ){
+                opponentsTeamId.push(match.teamA._id)
                 return match.teamA
-            else if( match.teamB.players && match.teamB.players.every(playerNotFound) )
+            }
+            else if( isExistB === -1 && match.teamB.players && match.teamB.players.every(playerNotFound) ){
+                opponentsTeamId.push(match.teamB._id)
                 return match.teamB
+            }
             else
                 return null
         }).filter( team => team !== null )
 
-
-        res.send(opponentsTeam)
+        // remove duplicates team and send
+        
+        res.send(Array.from(new Set([...opponentsTeam])))
 
     } catch (error) {
         console.log(error)
