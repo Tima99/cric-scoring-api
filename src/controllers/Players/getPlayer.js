529d36874451016009ec3922cad9c3efb02ebb8d
Tima <80810016+Tima99@users.noreply.github.com>
2022-12-11 19:35:31 +0530

    Done :)

--- src/controllers/Players/getPlayer.js
+++ src/controllers/Players/getPlayer.js
@@ -4,9 +4,9 @@ import { Player } from "../../models";
 export async function getPlayer(req , res){
     try {
         const {_id} = req.params
-        
+        if( !mongoose.isValidObjectId(_id) ) return res.status(422).send("Invalid player id")
         const player = await Player.findById({_id: mongoose.Types.ObjectId(_id)})
-        if(!player) return res.status(401).send("Player not Found!")
+        if(!player) return res.status(422).send("Player not Found!")
 
         res.send(player)
     } catch (error) {
