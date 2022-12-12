9e9ba129e4348f9e7aadaad1888cfdfb381b6ef7
Tima <80810016+Tima99@users.noreply.github.com>
2022-12-12 13:07:38 +0530

    Ready to go!

--- src/controllers/Players/getPlayer.js
+++ src/controllers/Players/getPlayer.js
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
 import { Player } from "../../models";
+import {validate} from "../../services"
 
 export async function getPlayer(req , res){
     try {
         const {_id} = req.params
-        if( !mongoose.isValidObjectId(_id) ) return res.status(422).send("Invalid player id")
-        const player = await Player.findById({_id: mongoose.Types.ObjectId(_id)})
+        let player = null
+        if( mongoose.isValidObjectId(_id) ){
+            player = await Player.findById({_id: mongoose.Types.ObjectId(_id)})
+        }
+        else if(validate.email(_id)){
+            player = await Player.findOne({email: _id})
+        }
+
         if(!player) return res.status(422).send("Player not Found!")
 
         res.send(player)
