7d2d2c766eb58ebd7e48b37e96a694dff86d3b5d
Tima <80810016+Tima99@users.noreply.github.com>
2022-12-13 17:25:39 +0530

    Done

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import {getRoutes, postRoutes, protectedRoutes} from "./routes/index.js"
 import { authenticate } from "./middlewares/index.js"
-import http from "http"
-import socket, {socketConnect} from "./sockets.js"
+// import http from "http"
+import socket from "./sockets.js"
 
 const app = express()
-const server = http.createServer(app)
 
 const corsOptions = {
     origin : ['https://cric-scoring.netlify.app', "http://localhost:3000"],
@@ -23,7 +22,7 @@ app.use('/api', getRoutes)
 app.use('/api', postRoutes)
 app.use('/api', authenticate, protectedRoutes)
 
-app.listen(PORT, () => console.log(`🌍 Server listening on http://localhost:${PORT}/api/`) )
+const server = app.listen(PORT, () => console.log(`🌍 Server listening on http://localhost:${PORT}/api/`) )
 socket.listen(server, () => console.log(`🔌 Socket Connected!`))
 
 mongoose.connect(DB_URL)
