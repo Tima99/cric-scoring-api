cfc8ea27065250304bfa7432ebed723ff8abfc96
Tima <80810016+Tima99@users.noreply.github.com>
2022-12-14 15:01:28 +0530

    OK:)

--- controllers/matches/myMatches.js
+++ controllers/matches/myMatches.js
@@ -11,6 +11,7 @@ export async function getMyMatches(req, res, next, ){
         const myMatches = matches.filter( (match, i) => {
             let isMyTeam = match.teamA.players.findIndex( ply => ply._id == email)
             isMyTeam = isMyTeam < 0 && match.teamB.players.findIndex( ply => ply._id == email)
+            isMyTeam = isMyTeam < 0 && match.scoringBy == email
             return isMyTeam > -1
         })
 
